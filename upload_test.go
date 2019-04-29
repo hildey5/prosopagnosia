70698d3c52d87695744c66a792249515e44679e9
Lucas Hilden <lucas.hilden@gmail.com>
2019-04-29 18:17:07 -0300

    This tests the upload of a image to an s3.

--- upload_test.go
+++ upload_test.go
@@ -24,3 +24,31 @@ func TestUploadCollectionFacesAddison(t *testing.T) {
 		t.Error("Testcase failed")
 	}
 }
+
+func TestUploadCollectionFacesAddison1(t *testing.T) {
+	err := uploadPhoto("faces.hilden", ".\\test\\facestest\\addison1.jpg", "addison1.jpg")
+	if err != nil {
+		t.Error("Testcase failed")
+	}
+}
+
+func TestUploadCollectionFacesAddison2(t *testing.T) {
+	err := uploadPhoto("faces.hilden", ".\\test\\facestest\\addison2.jpg", "addison2.jpg")
+	if err != nil {
+		t.Error("Testcase failed")
+	}
+}
+
+func TestUploadCollectionFacesLucas(t *testing.T) {
+	err := uploadPhoto("faces.hilden", ".\\test\\facestest\\lucas.jpg", "lucas.jpg")
+	if err != nil {
+		t.Error("Testcase failed")
+	}
+}
+
+func TestUploadCollectionFacesTammy1(t *testing.T) {
+	err := uploadPhoto("faces.hilden", ".\\test\\facestest\\tammy1.jpg", "tammy1.jpg")
+	if err != nil {
+		t.Error("Testcase failed")
+	}
+}
